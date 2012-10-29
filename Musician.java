a46906c16fa13d657451f70fc7dbee150dbe9881
mionisation <mionisation@gmail.com>
2012-10-29 21:12:31 +0100

    added assertions in Musician

--- Musician.java
+++ Musician.java
@@ -63,9 +63,9 @@ public class Musician {
 		return songs.getAt(date);
 	}
 	
-	/*
-	 * Returns a response on a specific Event. If there is none,
-	 * the Response returns false with a "No current response" message
+	/**
+	 * @param e the specific event we want a response on
+	 * @return returns the response if there is one on e in responses; else returns a negative response
 	 */
 	public EventResponse getResponse(Event e) {
 		for(EventResponse r : responses){
@@ -74,19 +74,30 @@ public class Musician {
 		}
 		return new EventResponse(e, false, "No current response");
 	}
-	
+	/**
+	 * adds a new Response to responses
+	 * @param e the Event on which a Response is created on
+	 * @param decision a boolean which represents the decision of the musician
+	 * on the Event (negative decision == false; positive decision == true)
+	 * @param message a String to clarify the decision
+	 * message and decision should be coherent and reasonable (e.g. if the message is "Sounds good,
+	 * I have time!" and decision == false is not reasonable and confusing)
+	 */
 	public void addResponse(Event e, boolean decision, String message) {
 		responses.add(new EventResponse(e, decision, message));
 	}
 
-	/*
+	/**
 	 * Musician sees changed or removed Event entries
 	 * on Notification List
+	 * @param e an EventProposal on a changed or removed Event entry
 	 */
 	public void eventNotification(EventProposal e) {
 		proposals.add(e);
 	}
-	
+	/**	
+	 * @return a reference on a list with the collected EventProposals
+	 */
 	public List<EventProposal> getProposals() {
 		return proposals;
 	}
