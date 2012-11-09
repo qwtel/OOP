593af6129ca6d711918ce37613b22dd229a16a20
Michael Ion <mionisation@Ideenmaschine.(none)>
2012-11-09 16:56:33 +0100

    ClearBox

---
+++ ClearBox.java
@@ -0,0 +1,11 @@
+/**
+ * ClearBox
+ * 
+ * @author Mihi
+ * @version 0.1
+ */
+public abstract class ClearBox extends AbstractBox {
+	public ClearBox(double width, double height){
+		super(width, height, '*', ' ' );
+	}
+}
