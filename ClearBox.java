23dc8e794114ba17e22c5a10eefdb5bba40e1f34
Florian Klampfer <f.klampfer@gmail.com>
2012-11-09 17:25:10 +0100

    implemented DarkBox, minor style changes

--- ClearBox.java
+++ ClearBox.java
@@ -6,8 +6,9 @@
  */
 public class ClearBox extends AbstractBox {
 	private double ratio;
-	public ClearBox(double width, double height){
-		super(width, height, '*', ' ' );
+
+	public ClearBox(double width, double height) {
+		super(width, height, '*', ' ');
 		ratio = width/height;
 	}
 
