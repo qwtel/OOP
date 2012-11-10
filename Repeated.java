09b34ecb29f5d5ef8d5596733034dd18d1789704
Florian Klampfer <f.klampfer@gmail.com>
2012-11-10 11:54:04 +0100

    outline for Repeated and Scaled

---
+++ Repeated.java
@@ -0,0 +1,12 @@
+/**
+ * Repeated
+ *
+ * TODO: Beschreibung
+ * 
+ * @author TODO
+ */
+public class Repeated<P> extends AbstractPictArray<P> {
+	public Repeated(P[][] content) {
+		super(content);		
+	}
+}
---
+++ Scaled.java
@@ -0,0 +1,12 @@
+/**
+ * Scaled
+ *
+ * TODO: Beschreibung
+ * 
+ * @author TODO
+ */
+public class Scaled<P extends Pict> extends AbstractPictArray<P> {
+	public Scaled(P[][] content) {
+		super(content);		
+	}
+}
