8d0ea39308a5c69d808110438d9a45483d41919f
Florian Klampfer <f.klampfer@gmail.com>
2012-11-19 00:24:47 +0100

    very short orderedset

--- OrderedSet.java
+++ OrderedSet.java
@@ -15,48 +15,8 @@
  */
 public class OrderedSet<E extends Shorter<? super E>> extends Set<E> {
 
-	/**
-	 * Nimmt ein Argument, das in die Menge eingefügt wird.
-	 *
-	 * Wird in der richtigen Stelle in der Liste eingefügt, welche mittels
-	 * shorter() bestimmt wird. Die Liste bleibt immer sortiert.
-	 *
-	 * Mehrere gleiche Elemente dürfen in der Menge sein (aber nicht mehrere 
-	 * identische).
-	 *
-	 * @param e Ein Element das sich noch nicht im Set befindet.
-	 * @return false, wenn sich das Element bereits in der Liste befindet.
-	 */
 	@Override
-	public boolean insert(E e) {
-		Node<E> newNode = new Node<E>(e);
-		if(root == null) {
-			root = newNode;
-		}
-		else {
-			Node<E> prev = null;
-			Node<E> curr = root;			
-			while(curr != null) {
-				if(curr == newNode) {
-					return false;
-				}
-				else if(newNode.elem.shorter(curr.elem)) {
-					if(prev != null) {
-						prev.next = newNode;
-					}
-					else {
-						root = newNode;
-					}
-					newNode.next = curr;
-					return true;
-				}
-			
-				prev = curr;
-				curr = prev.next;
-			}
-			prev.next = newNode;
-		}
-		return true;
-		
+	protected boolean insertHere(Node<E> curr, Node<E> newNode) {
+    	return (newNode.key.shorter(curr.key));
 	}
 }
