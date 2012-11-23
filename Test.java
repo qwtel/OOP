f2c8c75a4e00335c598f7b09b6fa221ffb28b80b
JohannesLaptop <send@johannesdeml.com>
2012-11-23 09:00:43 +0100

    sorry, forgot to put Test back in

---
+++ Test.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Test
+ * 
+ * Handels all TestCases, includes main method.
+ * 
+ * @author Florian Klampfer, Michael Ion, Johannes Deml
+ */
+public class Test {
+        public static void main(String[] args) throws Exception {
+
+                
+        }
+
+        private static int i = 0;
+        public static void doTest(String description, Object result, 
+                        Object expected) {
+
+                i++;
+                System.out.println("Test "+i+": "+description);
+
+                if (expected.equals(result)) {
+                        System.out.println("\nResult:");
+                        System.out.println(result);
+                        System.out.println("Test "+i+" passed.");
+                }
+                else {
+                        System.out.println("\nExpected result:");
+                        System.out.println(expected);
+
+                        System.out.println("Failed Result:");
+                        System.out.println(result);
+
+                        System.out.println("Test "+i+" failed!");
+                }
+
+                System.out.println(
+                                "------------------------------------------------------\n");
+
+        }
+}
