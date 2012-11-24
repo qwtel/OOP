1d8a58b900e40232d19ad32bb66d86c80633a655
Florian Klampfer <f.klampfer@gmail.com>
2012-11-24 17:58:13 +0100

    fixed tabs in test.java

--- Test.java
+++ Test.java
@@ -6,35 +6,35 @@
  * @author Florian Klampfer, Michael Ion, Johannes Deml
  */
 public class Test {
-        public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-                
-        }
+		
+	}
 
-        private static int i = 0;
-        public static void doTest(String description, Object result, 
-                        Object expected) {
+	private static int i = 0;
+	public static void doTest(String description, Object result, 
+			Object expected) {
 
-                i++;
-                System.out.println("Test "+i+": "+description);
+		i++;
+		System.out.println("Test "+i+": "+description);
 
-                if (expected.equals(result)) {
-                        System.out.println("\nResult:");
-                        System.out.println(result);
-                        System.out.println("Test "+i+" passed.");
-                }
-                else {
-                        System.out.println("\nExpected result:");
-                        System.out.println(expected);
+		if (expected.equals(result)) {
+			System.out.println("\nResult:");
+			System.out.println(result);
+			System.out.println("Test "+i+" passed.");
+		}
+		else {
+			System.out.println("\nExpected result:");
+			System.out.println(expected);
 
-                        System.out.println("Failed Result:");
-                        System.out.println(result);
+			System.out.println("Failed Result:");
+			System.out.println(result);
 
-                        System.out.println("Test "+i+" failed!");
-                }
+			System.out.println("Test "+i+" failed!");
+		}
 
-                System.out.println(
-                                "------------------------------------------------------\n");
+		System.out.println(
+				"------------------------------------------------------\n");
 
-        }
+	}
 }
