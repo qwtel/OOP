9a1e4259c487619f4acb23b3f285e478b0bb4009
Florian Klampfer <f.klampfer@gmail.com>
2012-11-28 11:49:34 +0100

    clean slate

--- SoftwareStufe2.java
+++
@@ -1,8 +0,0 @@
-
-public class SoftwareStufe2 extends SoftwareStufe{
-
-	@Override
-	boolean fuerHilfskraft(RoboShop shop, Hilfskraft android) { 
-		return android.aktor.fuerBediener(shop, android);
-	}
-}
--- SoftwareStufe5.java
+++
@@ -1,8 +0,0 @@
-
-public class SoftwareStufe5 extends SoftwareStufe{
-
-	@Override
-	boolean fuerKaempfer(RoboShop shop, Kaempfer android) {
-		return android.aktor.fuerKaempfer(shop, android);
-	}
-}
