c71d25d5da2d4a44e0f2a6f49c4a1b89abc071da
Michael Ion <me@michaelion.net>
2012-12-09 18:43:06 +0100

    annotations in geraete

--- GeraetDuenger.java
+++ GeraetDuenger.java
@@ -1,3 +1,5 @@
+import javax.xml.ws.soap.MTOM;
+
 
 /**
  * GeraetDuenger hat immer eine Infomation ueber die Anzahl der Kapazitaet des
@@ -6,14 +8,16 @@
  * @author Michael Ion
  *
  */
-
+@ClassAuthor(who="Michael Ion")
 public class GeraetDuenger implements Geraet {
+	
 	private float kapazitaet;
 	
 	public GeraetDuenger(float kapazitaet) {
 		this.kapazitaet = kapazitaet;
 	}
 	@Override
+	@MethodAuthor(who="Michael Ion")
 	public Number getZahl() {
 		return kapazitaet;
 	}
