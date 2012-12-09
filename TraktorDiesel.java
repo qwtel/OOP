65cb49349475006b030527025e81b6299ba6c25c
JohannesLaptop <send@johannesdeml.com>
2012-12-09 18:40:20 +0100

    Annotations for Tarator*

--- TraktorDiesel.java
+++ TraktorDiesel.java
@@ -5,11 +5,15 @@ public class TraktorDiesel extends Traktor {
 		super(geraet);
 		verbrauch = 0;
 	}
+	
 	@Override
+	@MethodAuthor(who = "Michael Ion")
 	public Number getTreibstoffverbrauch() {
 		return verbrauch;
 	}
+	
 	@Override
+	@MethodAuthor(who = "Michael Ion")
 	public void erhoeheVerbrauchtenTreibstoff(Number verbraucht) {
 		verbrauch += verbraucht.intValue();
 	}
