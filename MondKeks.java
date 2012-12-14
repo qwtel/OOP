4ed6af644ed898b1a7cf92c536c75ec1b05de3b4
Michael <me@michaelion.net>
2012-12-14 14:04:27 +0100

    keksi

---
+++ MondKeks.java
@@ -0,0 +1,8 @@
+/**
+ * Ein Keks in der Form eines Mondes
+ */
+public class MondKeks {
+	public MondKeks(Fuellung fuellung) {
+		super(fuellung);
+	}
+}
---
+++ RundKeks.java
@@ -0,0 +1,8 @@
+/**
+ * Ein Keks mit einer runden Form
+ */
+public class RundKeks extends Keks {
+	public RundKeks(Fuellung fuellung) {
+		super(fuellung);
+	}
+}
